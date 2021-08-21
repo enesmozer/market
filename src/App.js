a93e2d17484e7364559beea07e5c7fd017a5376d
Enes Malik Özer <enesmozer@gmail.com>
2021-08-21 11:38:31 +0300

    changed card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './styles/App.scss';
 import Header from './components/Header/Header';
+import SortingCard from './components/SortingCard/SortingCard';
+import FilterCard from './components/FilterCard/FilterCard';
 
 function App() {
   return (
     <div className="App">
       <Header cost={70} />
+      <SortingCard />
+      <FilterCard />
+      <FilterCard />
     </div>
   );
 }
