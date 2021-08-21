6caf96389bf79fb2ef6083a57130fb1bb11830e7
Enes Malik Özer <enesmozer@gmail.com>
2021-08-21 19:14:45 +0300

    product list component added

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,21 +1,38 @@
+import Product from '../Base/Product/Product';
 import './productList.scss';
 
 function Products() {
   return (
-    <main className="products">
-      <div className="products-header">
-        <div className="title"> Products</div>
-        <div className="buttons">
+    <main className="productList">
+      <div className="productList-header">
+        <div className="productList-title"> Products</div>
+        <div className="productList-buttons">
           <button className="button" type="button">
             mug
           </button>
-          <button className="button" type="button">
+          <button className="button active" type="button">
             shirt
           </button>
         </div>
       </div>
-      {/* <div className="products-main">
-      </div> */}
+      <div className="productList-main">
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+        <Product name="Office Mugs" cost={14.99} />
+      </div>
     </main>
   );
 }
