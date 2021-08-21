9f66e7435f9d3762e42323c867832f763bcef0eb
Enes Malik Özer <enesmozer@gmail.com>
2021-08-22 01:56:55 +0300

    request for company list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCompanies } from './redux/actions/index';
 import Header from './components/Header/Header';
 import SortingCard from './components/SortingCard/SortingCard';
 import FilterCard from './components/FilterCard/FilterCard';
@@ -6,14 +9,22 @@ import Basket from './components/Basket/Basket';
 import './styles/App.scss';
 
 function App() {
+  const dispatch = useDispatch();
+  const companies = useSelector((state) => state.companies.companies);
+  // const loading = useSelector((state) => state.companies.loading);
+  // const error = useSelector((state) => state.companies.error);
+
+  useEffect(() => {
+    dispatch(getCompanies());
+  }, []);
   return (
     <div className="App">
       <Header cost={70} />
       <div className="container">
         <div className="filters">
           <SortingCard />
-          <FilterCard />
-          <FilterCard />
+          <FilterCard data={companies} />
+          {/* <FilterCard data={companies} /> */}
         </div>
         <div className="products">
           <ProductList />
