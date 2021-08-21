454a20299c01a96107e6f32de24318a1e6461198
Enes Malik Özer <enesmozer@gmail.com>
2021-08-21 22:23:24 +0300

    saga middle ware and store created

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,13 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import companies from './reducers/companies';
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
 const reducers = combineReducers({
   companies,
 });
+const store = createStore(reducers);
 
 export default reducers;
