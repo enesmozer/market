a3f7e31eee30c7ff7d5687e0306db6e6c0de57a7
Enes Malik Özer <enesmozer@gmail.com>
2021-08-22 01:59:42 +0300

    filter card refactor with data props

--- src/components/FilterCard/FilterCard.js
+++ src/components/FilterCard/FilterCard.js
@@ -1,19 +1,30 @@
+import PropTypes from 'prop-types';
 import Card from '../Base/Card/Card';
 import Input from '../Base/Input/Input';
 import './filterCard.scss';
 
-function BrandsCard() {
+function BrandsCard({ data }) {
+  const dataList = data.map((item) => (
+    <Input
+      key={item.account}
+      label={item.name}
+      type="checkbox"
+      id={item.name}
+      value={item.name}
+    />
+  ));
   return (
     <Card title="Brands" className="filter">
       <Input type="text" label="Brands" />
-      <Input
-        label="Konopelski"
-        type="checkbox"
-        id="Konopelski"
-        value="Konopelski"
-      />
-      <Input label="Adidas" type="checkbox" id="Adidas" value="Adidas" />
+      <div className="list">{dataList}</div>
     </Card>
   );
 }
+BrandsCard.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 export default BrandsCard;
