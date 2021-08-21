4ef5e908875909e3c79bfcc8de42526d2548afd4
Enes Malik Özer <enesmozer@gmail.com>
2021-08-22 02:01:19 +0300

    product service added

--- src/services/products.js
+++ src/services/products.js
@@ -1,13 +1,13 @@
 import http from '../utils/http';
 
 const url = process.env.REACT_APP_API_URL;
-const CompanyService = {
+const ProductService = {
   async list() {
-    const response = await http.get(`${url}companies`);
+    const response = await http.get(`${url}products`);
     if (response.status === 200) {
       return response.data;
     }
     throw response.error;
   },
 };
-export default CompanyService;
+export default ProductService;
