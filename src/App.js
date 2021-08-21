58826f8c502c19d491c90e1f626b526181765194
Enes Malik Özer <enesmozer@gmail.com>
2021-08-21 19:15:28 +0300

    new components applied

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './styles/App.scss';
 import Header from './components/Header/Header';
 import SortingCard from './components/SortingCard/SortingCard';
 import FilterCard from './components/FilterCard/FilterCard';
+import ProductList from './components/ProductList/ProductList';
+import Basket from './components/Basket/Basket';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header cost={70} />
-      <SortingCard />
-      <FilterCard />
-      <FilterCard />
+      <div className="container">
+        <div className="filters">
+          <SortingCard />
+          <FilterCard />
+          <FilterCard />
+        </div>
+        <div className="products">
+          <ProductList />
+        </div>
+        <div className="product-basket">
+          <Basket />
+        </div>
+      </div>
     </div>
   );
 }
