1a1aadada3f485020ab8a2554bc8ac031765edb2
Enes Malik Özer <enesmozer@gmail.com>
2021-08-22 16:37:42 +0300

    pagination imolemented

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,24 +1,66 @@
-/* eslint-disable max-len */
+/* eslint-disable no-debugger */
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Product from '../Base/Product/Product';
+import Pagination from '../Pagination/Pagination';
 import './productList.scss';
 
 function ProductList({ list }) {
-  const products = list.map((product) => <Product key={product.added} name={product.name} cost={product.price} />);
+  const [PageSize] = useState(16);
+  const [currentPage, setCurrentPage] = useState(1);
+  const firstPageIndex = (currentPage - 1) * PageSize;
+  const lastPageIndex = firstPageIndex + PageSize;
+  const [currentProducts, setcurrentProducts] = useState([]);
+  useEffect(() => {
+    setcurrentProducts(list.slice(firstPageIndex, lastPageIndex));
+  }, [list]);
+
+  const filter = (e, key) => {
+    document.querySelectorAll('.button').forEach((item) => {
+      item.classList.remove('active');
+    });
+    e.currentTarget.classList.add('active');
+    const items = list.filter((item) => item.itemType === key);
+    setcurrentProducts(items);
+  };
+
   return (
     <main className="productList">
       <div className="productList-header">
         <div className="productList-title"> Products</div>
         <div className="productList-buttons">
-          <button className="button" type="button">
+          <button
+            className="button"
+            type="button"
+            onClick={(e) => filter(e, 'mug')}
+          >
             mug
           </button>
-          <button className="button active" type="button">
+          <button
+            className="button"
+            type="button"
+            onClick={(e) => filter(e, 'shirt')}
+          >
             shirt
           </button>
         </div>
       </div>
-      <div className="productList-main">{products}</div>
+      <div className="productList-main">
+        {currentProducts.map((product) => (
+          <Product
+            key={product.added}
+            name={product.name}
+            cost={product.price}
+          />
+        ))}
+      </div>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={list.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </main>
   );
 }
@@ -26,7 +68,7 @@ ProductList.propTypes = {
   list: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      cost: PropTypes.number.isRequired,
+      cost: PropTypes.number,
     }),
   ).isRequired,
 };
