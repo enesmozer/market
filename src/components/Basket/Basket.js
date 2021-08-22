c8ec7e7d442f9697b6b234a4940e23e3fb3a2b66
Enes Malik Özer <enesmozer@gmail.com>
2021-08-22 17:17:12 +0300

    cart props and getTotal function added

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,26 +1,53 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import './basket.scss';
 import minus from '../../assets/minus.svg';
 import plus from '../../assets/plus.svg';
 
-function Basket() {
+function Basket({ cart, setCart }) {
+  const getTotalSum = () => cart.reduce((sum, { price, quantity }) => sum + price * quantity, 0);
   return (
     <div className="basket">
-      <div className="basketProduct">
-        <div className="basketProduct-info">
-          <div className="basketProduct-name">Example Product</div>
-          <div className="basketProduct-cost"> 15 #</div>
+      {cart.map((item) => (
+        <div className="basketProduct">
+          <div className="basketProduct-info">
+            <div className="basketProduct-name">{item.name}</div>
+            <div className="basketProduct-cost">
+              {item.price}
+              {' '}
+            </div>
+          </div>
+          <div className="basketProduct-quantity">
+            <img
+              className="minus"
+              src={minus}
+              alt=""
+            />
+            <div className="basketProduct-quantity-number">{item.quantity}</div>
+            <img
+              className="plus"
+              src={plus}
+              alt=""
+            />
+          </div>
         </div>
-        <div className="basketProduct-quantity">
-          <img className="minus" src={minus} alt="" />
-          <div className="basketProduct-quantity-number">2</div>
-          <img className="plus" src={plus} alt="" />
-        </div>
-      </div>
+      ))}
+
       <div className="divider" />
-      <div className="total">15</div>
+      <div className="total">
+        ₺
+        {getTotalSum()}
+      </div>
     </div>
   );
 }
-Basket.propTypes = {};
+Basket.propTypes = {
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number,
+      quantity: PropTypes.number,
+    }),
+  ).isRequired,
+  setCart: PropTypes.func.isRequired,
+};
 export default Basket;
