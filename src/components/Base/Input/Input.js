fc3b7f109f3529300705110bbec121948972922d
Enes Malik Özer <enesmozer@gmail.com>
2021-08-22 20:50:37 +0300

    filter  datas of filter card

--- src/components/Base/Input/Input.js
+++ src/components/Base/Input/Input.js
@@ -2,19 +2,28 @@ import PropTypes from 'prop-types';
 import './input.scss';
 
 function Input({
-  label, type, value, name, id, setinputValue,
+  label, type, value, name, id, setinputValue, setTextValue,
 }) {
   const change = (e) => {
     setinputValue(e.currentTarget.value);
   };
+  const textChange = (e) => {
+    setTextValue(e.currentTarget.value);
+  };
   if (type === 'text') {
-    return <input type="text" placeholder={label} />;
+    return <input type="text" placeholder={label} onChange={textChange} />;
   }
   if (type === 'radio' || type === 'checkbox') {
     return (
       <label htmlFor={id} className="filter-input">
         <span className="label">{label}</span>
-        <input type={type} id={id} name={name} value={value} onChange={change} />
+        <input
+          type={type}
+          id={id}
+          name={name}
+          value={value}
+          onChange={change}
+        />
         <span className="checkmark" />
       </label>
     );
@@ -24,6 +33,7 @@ Input.propTypes = {
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   setinputValue: PropTypes.func,
+  setTextValue: PropTypes.func,
   value: PropTypes.string,
   name: PropTypes.string,
   id: PropTypes.string,
--- src/components/FilterCard/FilterCard.js
+++ src/components/FilterCard/FilterCard.js
@@ -1,21 +1,40 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Base/Card/Card';
 import Input from '../Base/Input/Input';
 import './filterCard.scss';
 
-function BrandsCard({ data }) {
-  const dataList = data.map((item) => (
+function BrandsCard({ data, filterCompanies, setFilterCompanies }) {
+  const [company, setCompany] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredCompanies, setFilteredCompanies] = useState([]);
+  useEffect(() => {
+    setFilteredCompanies(data);
+  }, [data]);
+  if (company) {
+    if (!filterCompanies.some((item) => item === company)) {
+      filterCompanies.push(company);
+    }
+    setFilterCompanies(filterCompanies);
+  }
+  useEffect(() => {
+    setFilteredCompanies(
+      data.filter((item) => item.name.toLowerCase().includes(searchInput.toLowerCase())),
+    );
+  }, [searchInput]);
+  const dataList = filteredCompanies.map((item) => (
     <Input
       key={item.account}
       label={item.name}
       type="checkbox"
       id={item.name}
       value={item.name}
+      setinputValue={setCompany}
     />
   ));
   return (
     <Card title="Brands" className="filter">
-      <Input type="text" label="Brands" />
+      <Input type="text" label="Brands" setTextValue={setSearchInput} />
       <div className="list">{dataList}</div>
     </Card>
   );
@@ -26,5 +45,10 @@ BrandsCard.propTypes = {
       name: PropTypes.string.isRequired,
     }),
   ).isRequired,
+  setFilterCompanies: PropTypes.func.isRequired,
+  filterCompanies: PropTypes.arrayOf(PropTypes.shape([])),
+};
+BrandsCard.defaultProps = {
+  filterCompanies: [],
 };
 export default BrandsCard;
